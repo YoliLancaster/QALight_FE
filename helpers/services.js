1517c6e3f2682f87197bfc64a827cc9efe57ed6e
Yoli <yoli.lancaster@gmail.com>
2024-04-20 18:53:09 +0300

    fill about section with data

--- helpers/services.js
+++ helpers/services.js
@@ -1,38 +1,44 @@
-[
+export const services = [
   {
     id: 1,
     service: "Training",
     description:
       "Correct behaviors, educate, have fun and strengthen your relationship with your pet.",
+    image: "pet-house-1",
   },
   {
     id: 2,
     service: "Food",
     description:
       "Discover balanced diets, learn about homemade diets, choose the best food and much more.",
+    image: "pet-food-2",
   },
   {
     id: 3,
     service: "Adoption",
     description:
       "Discover charming names, tips, what to do and everything you need to know to adopt a pet.",
+    image: "loving-3",
   },
   {
     id: 4,
     service: "Health",
     description:
       "Learn everything you need to know about your pet's health and protect your pet from illnesses.",
+    image: "animal-care-4",
   },
   {
     id: 5,
     service: "Care",
     description:
       "Understand heat, pregnancy, dental hygiene, ear care and much more.",
+    image: "animal-day-5",
   },
   {
     id: 6,
     service: "Curiosities",
     description:
       "Learn tips, discover species in danger of extinction, learn about different breeds and much more.",
+    image: "teasing-stick-6",
   },
 ];
---
+++ js/services.js
@@ -0,0 +1,30 @@
+import { services } from "../helpers/services.js";
+
+const grid = document.querySelector(".grid");
+
+grid.innerHTML = "";
+
+console.log("Services:", services);
+
+const cardItem = createCard(services);
+grid.innerHTML = cardItem;
+
+function createCard(data) {
+  return data
+    .map(
+      ({ id, service, description, image }) =>
+        `<div class="card-container">
+            <div class="img-wrapper">
+                <img src="../img/${image}.png" alt="" class="card-icon" />
+              </div>
+
+              <div class="card-bottom">
+                <h3 class="card-title">${service}</h3>
+                <p class="card-text">
+                  ${description}
+                </p>
+              </div>
+              </div>`
+    )
+    .join("");
+}
